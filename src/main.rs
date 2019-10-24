79272554b031758583fd5a01ebde46092ae2278a
Arnaud Castellanos Galea <poderosoarnaud@gmail.com>
2019-10-24 17:52:00 +0800

    First Commit

---
+++ src/main.rs
@@ -0,0 +1,300 @@
+
+#![feature(test)]
+#![allow(unused, dead_code)]
+
+extern crate test;
+
+use std::time::{Instant, Duration};
+use std::collections::HashMap;
+use std::sync::atomic::{AtomicUsize, Ordering::Relaxed};
+use std::sync::{Arc, RwLock};
+use std::cell::RefCell;
+
+use parking_lot::RwLock as PLLock;
+use crossbeam::thread;
+use arc_swap::ArcSwap;
+use evmap::{ReadHandle, ReadHandleFactory, WriteHandle};
+use dashmap::DashMap;
+
+const THREADS: usize = 8;
+const MAP_SIZE: usize = 10000;
+const BENCH_WRITES: bool = true;
+
+const DURATION: Duration = Duration::from_secs(5);
+
+#[derive(Debug, Hash, PartialEq, Eq, Clone)]
+struct Foo {
+    data: [usize; 16]
+}
+
+impl Foo {
+    fn new (n: usize) -> Self {
+        Self { data: [n; 16] }
+    }
+
+    // Should return n*8 or (n-1)*8
+    fn sum (&self) -> usize {
+        let mut n = self.data[0];
+        for i in 1..16 {
+            n += self.data[i];
+        }
+        n
+    }
+}
+
+trait Mappy <'a>: Send + Sync {
+    type Reader: MappyReader<'a>;
+    //type Iter: Iterator<Item=Foo>;
+
+    fn new () -> Self;
+    fn name () -> &'static str;
+    fn set (&self, i: usize, val: Foo);
+    fn reader (&'a self) -> Self::Reader;
+    //fn iter (&self) -> Self::Iter;
+}
+
+trait MappyReader<'a>: Send {
+    fn get <F: FnOnce(&Foo)> (&self, i: usize, f: F);
+}
+
+//        parking_lot
+//===========================
+
+struct MyPLLock (PLLock<HashMap<usize, Foo>>);
+unsafe impl Send for MyPLLock {}
+unsafe impl Sync for MyPLLock {}
+impl <'a> Mappy<'a> for MyPLLock {
+    type Reader = &'a MyPLLock;
+    fn new () -> Self {
+        Self(PLLock::new(
+            (0..MAP_SIZE).into_iter().map(|i| {
+                (i, Foo::new(i))
+            }).collect()
+        ))
+    }
+
+    fn set (&self, i: usize, foo: Foo) {
+        self.0.write().insert(i, foo);
+    }
+
+    fn reader (&'a self) -> &'a Self { &self }
+
+    fn name () -> &'static str { "parking_lot::RwLock<HashMap>" }
+}
+impl <'a> MappyReader<'a> for &MyPLLock {
+    fn get <F: FnOnce(&Foo)>(&self, i: usize, f: F) {
+        self.0.read().get(&i).map(f);
+    }
+}
+
+//            std
+//===========================
+
+struct MyRwLock (RwLock<HashMap<usize, Foo>>);
+unsafe impl Send for MyRwLock {}
+unsafe impl Sync for MyRwLock {}
+impl <'a> Mappy<'a> for MyRwLock {
+    type Reader = &'a Self;
+
+    fn new () -> Self {
+        Self(RwLock::new(
+            (0..MAP_SIZE).into_iter().map(|i| {
+                (i, Foo::new(i))
+            }).collect()
+        ))
+    }
+
+    /*fn get <F: FnOnce(&Foo)>(&self, i: usize, f: F) {
+        self.0.read().map(|lock| lock.get(&i).map(f));
+    }*/
+
+    fn set (&self, i: usize, foo: Foo) {
+        self.0.write().map(|mut lock| lock.insert(i, foo));
+    }
+
+    fn reader (&'a self) -> &'a Self { &self }
+
+    fn name () -> &'static str { "std::sync::RwLock<HashMap>" }
+}
+impl <'a> MappyReader<'a> for &MyRwLock {
+    fn get <F: FnOnce(&Foo)>(&self, i: usize, f: F) {
+        self.0.read().map(|lock| lock.get(&i).map(f));
+    }
+}
+
+//          ArcSwap
+//===========================
+
+struct MyArcSwap (ArcSwap<HashMap<usize, Foo>>);
+unsafe impl Send for MyArcSwap {}
+unsafe impl Sync for MyArcSwap {}
+impl <'a> Mappy<'a> for MyArcSwap {
+    type Reader = &'a Self;
+
+    fn new () -> Self {
+        Self(ArcSwap::new(Arc::new(
+            (0..MAP_SIZE).into_iter().map(|i| {
+                (i, Foo::new(i))
+            }).collect()
+        )))
+    }
+
+    fn set (&self, i: usize, foo: Foo) {
+        let mut cloned = self.0.load().as_ref().clone();
+        cloned.insert(i, foo);
+        self.0.store(Arc::new(cloned));
+    }
+
+    fn reader (&'a self) -> &'a Self { &self }
+
+    fn name () -> &'static str { "ArcSwap<HashMap>" }
+}
+impl <'a> MappyReader<'a> for &MyArcSwap {
+    fn get <F: FnOnce(&Foo)>(&self, i: usize, f: F) {
+        self.0.load().get(&i).map(f);
+    }
+}
+
+//          Dashmap
+//===========================
+
+struct MyDashMap (DashMap<usize, Foo>);
+unsafe impl Send for MyDashMap {}
+unsafe impl Sync for MyDashMap {}
+impl <'a> Mappy<'a> for MyDashMap {
+    type Reader = &'a Self;
+
+    fn new () -> Self {
+        Self(
+            (0..MAP_SIZE).into_iter().map(|i| {
+                (i, Foo::new(i))
+            }).collect()
+        )
+    }
+
+    fn set (&self, i: usize, foo: Foo) {
+        self.0.insert(i, foo);
+    }
+
+    fn reader (&'a self) -> &'a Self { &self }
+
+    fn name () -> &'static str { "DashMap" }
+}
+impl <'a> MappyReader<'a> for &MyDashMap {
+    fn get <F: FnOnce(&Foo)>(&self, i: usize, f: F) {
+        self.0.get(&i).map(|guard| f(&guard));
+    }
+}
+
+//          evmap
+//===========================
+
+struct MyEvmap {
+    factory: ReadHandleFactory<usize, Box<Foo>>,
+    writer: RefCell<WriteHandle<usize, Box<Foo>>>,
+}
+unsafe impl Send for MyEvmap {}
+unsafe impl Sync for MyEvmap {}
+impl <'a> Mappy<'a> for MyEvmap {
+    type Reader = MyEvmapReader;
+
+    fn new () -> Self {
+        let (reader, mut writer) = evmap::new();
+        writer.extend(
+            (0..MAP_SIZE).map(|i| {
+                (i, Box::new(Foo::new(i)))
+            })
+        );
+        writer.refresh();
+        Self { factory: reader.factory(), writer: RefCell::new(writer) }
+    }
+
+    fn set (&self, i: usize, foo: Foo) {
+        let mut writer = self.writer.borrow_mut();
+        writer.update(i, Box::new(foo));
+        writer.refresh();
+    }
+
+    fn reader (&'a self) -> MyEvmapReader { MyEvmapReader(self.factory.handle()) }
+
+    fn name () -> &'static str { "evmap<Box>" }
+}
+struct MyEvmapReader (ReadHandle<usize, Box<Foo>>);
+impl <'a> MappyReader<'a> for MyEvmapReader {
+    fn get <F: FnOnce(&Foo)>(&self, i: usize, f: F) {
+        self.0.get_and(&i, |arr| {
+            f(arr[0].as_ref())
+        });
+    }
+}
+
+//         benchmark
+//===========================
+
+
+fn bench <Map> () -> std::thread::JoinHandle<()> where for<'a> Map: Mappy<'a> {
+    std::thread::spawn(|| {
+        let map = Map::new();
+        let read_count = AtomicUsize::new(0);
+        let write_count = AtomicUsize::new(0);
+
+        thread::scope(|scope| {
+            let end = Instant::now() + DURATION;
+
+            // So that the reference is copied, instead of the map itself, which is not copy
+            let map = &map;
+            let read_count = &read_count;
+            let write_count = &write_count;
+
+            for thread_i in 0..THREADS {
+
+                let reader = map.reader();
+                scope.spawn(move |_| {
+                    let mut i = (thread_i*MAP_SIZE) / THREADS;
+
+                    while (Instant::now() < end) {
+                        reader.get(i, |foo| {
+                            test::black_box(foo);
+                        });
+
+                        read_count.fetch_add(1, Relaxed);
+                        i += 1;
+                        if (i >= MAP_SIZE) { i = 0; }
+                    }
+                });
+            }
+
+            if BENCH_WRITES {
+                scope.spawn(move |_| {
+                    let mut i = 0;
+
+                    while (Instant::now() < end) {
+                        map.set(i, Foo::new(i));
+
+                        write_count.fetch_add(1, Relaxed);
+                        i += 1;
+                        if (i >= MAP_SIZE) { i = 0; }
+                    }
+                });
+            }
+        });
+
+        println!("{}: {} reads, {} writes", Map::name(), read_count.load(Relaxed), write_count.load(Relaxed));
+    })
+}
+
+fn main() {
+    let start = Instant::now();
+    let end = start + DURATION;
+
+    let joins = vec![
+        bench::<MyPLLock>(),
+        bench::<MyRwLock>(),
+        bench::<MyArcSwap>(),
+        bench::<MyEvmap>(),
+        bench::<MyDashMap>(),
+    ];
+
+    // Wait for the threads to finish
+    for join in joins { join.join(); }
+}
